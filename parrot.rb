b8bb9b1e5da8d440a8bf99122feb4e720620029d
gentle-reference-3520 <ahill0592@gmail.com>
2018-09-19 15:18:55 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot(argument)
+  phrase =  "parrot #{argument}"
+  puts phrase
+  phrase
+end
+  # Create method `parrot` that outputs a given phrase and
 # returns the phrase
